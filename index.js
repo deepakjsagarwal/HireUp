001119b59eda68bf10d9f2e89348a833e12467e6
hireupworks <hireupworks@gmail.com>
2022-01-31 20:56:47 -0800

    Format index.js

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = 6060
 const methodOverride = require('method-override');
 const session = require('express-session')
 const flash = require('connect-flash')
 const ExpressError = require('./utils/ExpressError');
 const userRoutes = require('./routes/users')
-const {companies} = require('./public/javascripts/companies');
+const { companies } = require('./public/javascripts/companies');
 var cookieParser = require('cookie-parser');
-
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser());
 
 const firebase = require("firebase");
 const db = firebase.firestore();
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 const sessionConfig = {
     name: 'session',
@@ -34,39 +30,43 @@ const sessionConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-    //  secure: true,
-        expires: Date.now()+1000*60*60*24*7,
+        //  secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 app.use(session(sessionConfig))
 app.use(flash())
 
-app.use(async(req,res,next)=>{
+app.use(async (req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error')
     res.locals.currentUser = firebase.auth().currentUser;
-    // console.log("CUREENT USER",firebase.auth().currentUser);
-    next();   
+    next();
 })
 
 
-app.use('/',userRoutes)
-app.get('/',(req,res)=>{
-    res.render('home',{companies});
+app.use('/', userRoutes)
+app.get('/', (req, res) => {
+    res.render('home', { companies });
 })
 
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
 })
+
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something went wrong';
-    res.status(statusCode).render('error',{err});
+    res.status(statusCode).render('error', { err });
 })
 
-app.listen(6060, () => {
-    console.log("Listening on 6060")
+app.listen(PORT, () => {
+    console.log("Server is running on PORT", PORT)
 })
