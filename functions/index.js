0e08460d6389d77fc3a6f96d391afcb179cc39ce
ReferralProject <hireupworks@gmail.com>
2022-02-16 18:09:51 -0800

    add cors.

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,7 @@ const flash = require('connect-flash');
 const path = require('path');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
+const cors = require('cors')({ origin: true });
 
 const serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors);
 app.use(cookieParser('hireuptothemoon'));
 app.use(session({
     name: '__session',
